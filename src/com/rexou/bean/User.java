0fb21805d3e73c7b8c7afd7bcc247ea25baec911
rexou23 <qiushi23@qq.com>
2019-08-10 22:43:08 +0800

    idea first commit

---
+++ src/com/rexou/bean/User.java
@@ -0,0 +1,10 @@
+package com.rexou.bean;
+
+/**
+ * @author qiushi 2019/8/10
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("my name is jack");
+    }
+}
